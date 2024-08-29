63b5b920d7962a3567838e2a1bac96645e75120b
alexmllo <amitjansllorach1@gmail.com>
2024-08-29 15:54:54 +0200

    push

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,9 +2,6 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
         linters_by_ft = {
-            javascript = { "eslind_d" },
-            golang = { "golangci-lint" },
-            java = { "checkstyle" },
             c = { "cpplint" },
         },
     },
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,13 +3,9 @@ return {
         "nvim-treesitter/nvim-treesitter",
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, {
-                "go",
-                "java",
                 "cpp",
                 "css",
-                "dockerfile",
                 "rust",
-                "sql",
             })
         end,
     },
