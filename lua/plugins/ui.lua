85d3bca6b3c55dfd9b14cdcfcf249996260f50f6
alexmllo <amitjansllorach1@gmail.com>
2024-09-07 21:16:53 +0200

    push

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -55,35 +55,6 @@ return {
         },
     },
 
-    -- filename - catppuccin
-    -- {
-    --     "b0o/incline.nvim",
-    --     event = "BufReadPre",
-    --     priority = 1200,
-    --     config = function()
-    --         require("incline").setup({
-    --             highlight = {
-    --                 groups = {
-    --                     InclineNormal = { guibg = "#303270", guifg = "#a9b1d6" },
-    --                     InclineNormalNC = { guibg = "none", guifg = "#a9b1d6" },
-    --                 },
-    --             },
-    --             window = { margin = { vertical = 0, horizontal = 1 } },
-    --             hide = { cursorline = true },
-    --             render = function(props)
-    --                 local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-    --                 if vim.bo[props.buf].modified then
-    --                     filename = "[*]" .. filename
-    --                 end
-    --
-    --                 local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-    --
-    --                 return { { icon, guifg = color }, { " " }, { filename } }
-    --             end,
-    --         })
-    --     end,
-    -- },
-
     -- filename
     {
         "b0o/incline.nvim",
@@ -135,32 +106,45 @@ return {
     },
 
     -- statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = function(_, opts)
+            opts.options.theme = "solarized_dark"
+            local LazyVim = require("lazyvim.util")
+            opts.sections.lualine_c[4] = {
+                LazyVim.lualine.pretty_path({
+                    length = 0,
+                    relative = "cwd",
+                    modified_hl = "MatchParen",
+                    directory_hl = "",
+                    filename_hl = "Bold",
+                    modified_sign = "",
+                    readonly_icon = " 󰌾 ",
+                }),
+            }
+        end,
+    },
+
     -- {
     --     "nvim-lualine/lualine.nvim",
-    --     opts = function(_, opts)
-    --         local LazyVim = require("lazyvim.util")
-    --         opts.sections.lualine_c[4] = {
-    --             LazyVim.lualine.pretty_path({
-    --                 length = 0,
-    --                 relative = "cwd",
-    --                 modified_hl = "MatchParen",
-    --                 directory_hl = "",
-    --                 filename_hl = "Bold",
-    --                 modified_sign = "",
-    --                 readonly_icon = " 󰌾 ",
-    --             }),
-    --         }
-    --     end,
+    --     event = "VeryLazy",
+    --     opts = {
+    --         options = {
+    --             theme = "solarized_dark",
+    --         },
+    --     },
     -- },
 
     {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
+        "folke/zen-mode.nvim",
+        cmd = "ZenMode",
         opts = {
-            options = {
-                theme = "solarized_dark",
+            plugins = {
+                gitsigns = true,
+                tmux = true,
             },
         },
+        keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
     },
 
     {
